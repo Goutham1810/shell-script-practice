5bbe01419f3665f376b484487ab1abfff198e911
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-16 14:52:15 +0530

    Normal File

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1="Goutham"
+PERSON2="Krishna"
+
+echo "Hai $PERSON2 How are you..?"
+echo "I am good.. How about you $PERSON1..?"
+echo "What you are doing $PERSON2"
+echo "I am learning Devops currently"
