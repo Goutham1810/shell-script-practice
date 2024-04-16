240413a4471943c1cb3dfc07221bb407502a0228
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-16 17:06:21 +0530

    Normal File

--- 05-arrays.sh
+++ 05-arrays.sh
@@ -3,7 +3,7 @@
 MOVIES=("RRR" "GarudaVedgha" "Aaata")
 
 #print 1st item of array
-echo "First Movie $MOVIES[0]"
+echo "First Movie" $MOVIES[0]
 #print 2nd item of array
 echo "Second Movie ${MOVIES[1]}"
 #print all the array elements
