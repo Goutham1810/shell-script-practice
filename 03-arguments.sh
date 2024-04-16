2e60aa7478c000f899148d01fdddaeb39a5a1fae
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-16 15:21:02 +0530

    Normal File

---
+++ 03-arguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "Hai $PERSON1 How are you..?"
+echo "I am good.. How about you $PERSON2..?"
+echo "I too good.. Doing software job"
+echo "What you are doing $PERSON1"
+echo "I am learning Devops currently"
