c04dc59ad922bccaef3b506b745f0778d011746f
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-17 13:17:55 +0530

    shell-script-practice

--- 09-installation.sh
+++ 09-installation.sh
@@ -9,12 +9,20 @@ exit 1; # To manually exit
 else
 echo "You are good to execute the installation commands"
 fi
-dnf install mysqll -y
+dnf install mysql -y
 if [ $? -ne 0 ]
 then
 echo "The installation of mysql is failed"
 exit 1;
 else
 echo "The installation of mysql is successful"
+fi
+dnf install git -y
+if [ $? -ne 0 ]
+then
+echo "The installation of git is failed"
+exit 1;
+else
+echo "The installation of git is successful"
 fi
 
