bc2ea0ea01f140231e0157ddc6ede5990414c638
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-17 13:16:49 +0530

    shell-script-practice

--- 09-installation.sh
+++ 09-installation.sh
@@ -8,6 +8,13 @@ echo "Please run this user from sudo user"
 exit 1; # To manually exit 
 else
 echo "You are good to execute the installation commands"
-dnf install mysql -y
-dnf install git -y
 fi
+dnf install mysqll -y
+if [ $? -ne 0 ]
+then
+echo "The installation of mysql is failed"
+exit 1;
+else
+echo "The installation of mysql is successful"
+fi
+
