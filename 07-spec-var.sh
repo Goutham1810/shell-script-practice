f66f9ebed6e7e6a2c0b8278e096eff2aeb7185e4
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-17 12:29:17 +0530

    shell-script-practice

---
+++ 07-spec-var.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo "To print all the arguments/variables in a script: $@"
+echo "To count the number of arguments/variable in a script: $#"
+echo "To Display the script name in the console: $0"
+echo "To print current working directory: $PWD"
+echo "To print home directory: $HOME"
