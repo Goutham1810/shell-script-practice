f716ede05287669028bf6c1928eaaf323d9de434
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-17 12:23:57 +0530

    Shell Script Practice

--- 07-spec-varibles.sh
+++ 07-spec-varibles.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
+echo "To print current working directory: $PWD"
 echo "To print all the arguments/variables in a script: $@"
 echo "To count the number of arguments/variable in a script: $#"
 echo "To Display the script name in the console: $0"
-#echo "To Display the current working directory: ${PWD}"
-echo "To Display the home directory $HOME"
