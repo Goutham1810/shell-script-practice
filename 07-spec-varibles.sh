0400b5b3bc1c0ff3fa269f55a8785e796aecbf34
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-17 12:14:07 +0530

    Normal File

--- 07-spec-varibles.sh
+++ 07-spec-varibles.sh
@@ -2,4 +2,5 @@
 
 echo "To print all the arguments/variables in a script: $@"
 echo "To count the number of arguments/variable in a script: $#"
-echo "To Display the script name in the console: $0,$1"
+echo "To Display the script name in the console: $0"
+echo "To Display the current working directory: $pwd"
