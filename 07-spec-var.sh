b75c900f8860279d5841ccd743d5056aa0e38c9e
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-17 12:34:02 +0530

    shell-script-practice

--- 07-spec-var.sh
+++ 07-spec-var.sh
@@ -7,6 +7,7 @@ echo "To Display the script name in the console: $0"
 echo "To print current working directory: $PWD"
 echo "To print home directory: $HOME"
 echo "To print the hostname: $HOSTNAME"
-echo "To print the display of processor id of current program: $$"
 echo "To print the username $USER"
 echo "To print the exit status $?"
+echo "To print the display of processor id of current program: $$"
+sleep 60
