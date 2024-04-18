51fe92db7b82b99aba571162c54443d274960c0e
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-18 16:55:11 +0530

    Installation Procedure Through Script

--- 12-Installation-procedure.sh
+++ 12-Installation-procedure.sh
@@ -14,18 +14,18 @@ VALIDATE()
 {
     if [ $1 -ne 0 ]
     then 
-        echo -e "$R $2.. Installation is failed.."
+        echo -e "$2.. Installation is $R failed..$N"
         exit 1;
     else
-        echo -e "$G $2.. Installation is success.."
+        echo -e "$2.. Installation is $G success..$N"
     fi    
 }
 
 if [ $USERID -eq 0 ]
 then
-echo -e "Connected To $R Sudo User $N"
+echo -e "Connected To $G Sudo User $N"
 else
-echo -e "Sudo access is required to install packages"
+echo -e "Sudo access is required to install packages $N"
 exit 1;
 fi
 
@@ -38,6 +38,6 @@ then
     echo "The $i packages are installed.. Hence we can ignore it.."
 else
     dnf install $i -y &>>$LOGFILE
-    VALIDATE $? "Installation of $i"
+    VALIDATE $? "Installation of $i $G"
 fi
 done
