a1e234df90b25b567bee106262ee4ea5c66d5baf
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-18 16:17:59 +0530

    Installation Procedure Through Script

--- 12-Installation-procedure.sh
+++ 12-Installation-procedure.sh
@@ -3,24 +3,41 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$R $2.. Installation is failed.."
+        exit 1;
+    else
+        echo -e "$G $2.. Installation is success.."
+    fi    
+}
 
 if [ $USERID -eq 0 ]
 then
-echo "Connected To Sudo User.."
+echo -e "Connected To $R Sudo User $N"
 else
-echo "Sudo access is required to install the packages"
+echo -e "Sudo access is required to install packages"
 exit 1;
 fi
 
 for i in $@
 do
-echo "Install the packages..$i"
+echo "Validating $i package"
 dnf list installed $i &>>$LOGFILE
 if [ $? -eq 0 ]
 then
     echo "The $i packages are installed.. Hence we can ignore it.."
 else
-    echo "The $i package is not installed.."
+    dnf install $i -y &>>$LOGFILE
+    VALIDATE $? "Installation of $i"
 fi
 done
