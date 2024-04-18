55f96a16b677b6ec9b68ad84addce99b164dfaa7
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-18 14:41:23 +0530

    Functions

---
+++ 10-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE()
+{
+    if [$? -ne o]
+    then 
+        echo "$2.. Installation is failed.."
+        exit 1;
+    else
+        echo "$2.. Installation is success.."
+    fi    
+}
+
+if [ $USERID -ne 0 ]  
+then
+echo "Please run this user from sudo user"
+exit 1; # To manually exit 
+else
+echo "You are good to execute the installation commands"
+fi
+dnf install mysql -y
+VALIDATE $? "Mysql"
+dnf install git -y
+VALIDATE $? "Github"
