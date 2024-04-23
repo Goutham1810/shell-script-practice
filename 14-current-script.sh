0020b43d4add67e8caf2011c76dde1eac76688a5
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-23 15:42:28 +0530

    calling script

--- 14-current-script.sh
+++ 14-current-script.sh
@@ -6,10 +6,10 @@ NAME="Goutham From Current Script"
 echo "Before calling other script, name: $NAME"
 echo "Process ID of current shell script: $$"
 
-#./15-other-script.sh
+./15-other-script.sh
 #Process ID of current shell script: 1509
 #Process ID of other script: 1510
 
-source ./15-other-script.sh
+#source ./15-other-script.sh
 
 echo "After calling other script, name: $NAME"
