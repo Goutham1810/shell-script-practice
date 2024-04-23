8c83dee52b4df78a9f4fbb70e37ccef5acfeb739
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-23 18:21:24 +0530

    Shell Script Set Command

--- 17-retention.sh
+++ 17-retention.sh
@@ -11,4 +11,15 @@ then
     echo "Directory exists.."
 else
     echo "Directory doesn't exists we have to create it.."
+    exit 1;
 fi
+
+# To find the file which are greater than 14 days
+
+FILES_LIST=$(find $DIRECTORY -name "*.log" -mtime +14)
+
+while IFS= read -r line
+do
+    echo "Deleting Files:"$line
+    #rm -rf $line
+done <<< $FILES_LIST
