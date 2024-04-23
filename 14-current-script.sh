80bca1114f252770fe759c8ff0e8ca213741a241
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-23 15:38:42 +0530

    calling script

--- 14-current-script.sh
+++ 14-current-script.sh
@@ -5,8 +5,10 @@ COURSE="DevOps from Current Script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-./15-other-script.sh
+#./15-other-script.sh
+#Process ID of current shell script: 1509
+#Process ID of other script: 1510
 
-#source ./15-other-script.sh
+source ./15-other-script.sh
 
 echo "After calling other script, course: $COURSE"
