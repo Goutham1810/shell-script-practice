9eaf61888667281e7f56389d81c47686841b6ab1
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-27 23:58:15 +0530

    Update 19-assignment1.sh
    daf

--- 19-assignment1.sh
+++ 19-assignment1.sh
@@ -1,20 +1,11 @@
 #Write a script that reads a text file and count the occurences of each word, displaying the top 5 most
 #frequent words along with their counts
 
-#filename="C:\Users\kalya\OneDrive\Documents\GitHub\shell-script-practice\sample.txt"
+filename="/home/ec2-user/shell-script-practice\sample.txt"
 #filecontent="Hi, this is goutham Currently working in SCB SCB is my second company holding an position of Data Engineer I want to move as Devops Engineer."
 
-declare -A words
-
+read -p $filename
 while read line
 do
-    for word in $line
-    do
-        ((words[$word]++))
-    done
-done
-
-for i in "${!words[@]}"
-do
-    echo "$i:" "${words[$i]}"
-done
+    echo $line
+done < $filename
