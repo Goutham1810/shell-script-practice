253569e96756708bc6572b5125cf1b3f24228bec
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-27 13:41:59 +0530

    Update 18-DiskUsage.sh

--- 18-DiskUsage.sh
+++ 18-DiskUsage.sh
@@ -6,7 +6,7 @@ while IFS=read -r line
 do
 USAGE=$(echo $line | awk -F " " '{print $5}' | cut -d "%" -f1)
 FOLDER_NAME=$(echo $line | awf -F " " '{print %NF}')
-if[USAGE -ge $DISK_THRES]
+if[$USAGE -ge $DISK_THRES]
 then
     MESSAGE+=echo -e "The $FOLDER_NAME -- $USAGE Crossed Threshold Value"
 else
