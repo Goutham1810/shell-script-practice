6316fffdc5c1a2c9cedb18ffe8affc452a704dd7
Somu Sundara Goutam <ssgoutham22@gmail.com>
2024-04-28 00:00:04 +0530

    Update 19-assignment1.sh
    adfad

--- 19-assignment1.sh
+++ 19-assignment1.sh
@@ -1,7 +1,7 @@
 #Write a script that reads a text file and count the occurences of each word, displaying the top 5 most
 #frequent words along with their counts
 
-filename="/home/ec2-user/shell-script-practice\sample.txt"
+filename="/home/ec2-user/shell-script-practice/sample.txt"
 #filecontent="Hi, this is goutham Currently working in SCB SCB is my second company holding an position of Data Engineer I want to move as Devops Engineer."
 
 read -p $filename
